d5b0a0a46d47cc5493ecbf9fc9834573a9c773e3
adeliafebriana152-beep <adelia.febriana152@gmail.com>
2026-06-10 11:32:45 +0800

    Refactor LimitBudgetException comments

--- LimitBudgetException.java
+++ LimitBudgetException.java
@@ -1,4 +1,4 @@
-public final class LimitBudgetException extends Exception { // emil di cek caca
+public final class LimitBudgetException extends Exception { // emil - Fitur Exception _di cek caca
     public LimitBudgetException(String message) {
         super(message);
     }
